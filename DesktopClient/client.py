32bb82c1543630eb8bb33c54de8730ff26e338b1
Сергей Пищулов <57267495+SergeyPishchulov@users.noreply.github.com>
2021-10-29 15:06:34 +0500

    Отправка сообщений из Desktop

--- DesktopClient/client.py
+++ DesktopClient/client.py
@@ -6,9 +6,15 @@ from DesktopClient.config import API_ID, API_HASH
 client = TelegramClient('Session in mixer client', API_ID, API_HASH)
 client.start()
 
-# await client.send_message('@Batislavich', """Ярославик, здорово,
-#  это сообщение из нашего питоновского телеграм-клиента""")
+# for dialog in client.iter_dialogs():
+#     print(dialog.title)
 
 
-for dialog in client.iter_dialogs():
-    print(dialog.title)
+async def main():
+    # Getting information about yourself
+    me = await client.get_me()
+    await client.send_message('+7 904 982 6823', "Привет, мам")
+
+
+with client:
+    client.loop.run_until_complete(main())
