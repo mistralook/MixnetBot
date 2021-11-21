23e6b26c28e81c5bfc7d6d50f71c039276e9e4c5
Сергей Пищулов <57267495+SergeyPishchulov@users.noreply.github.com>
2021-11-21 16:23:28 +0500

    fix space

--- Protocol/field_type.py
+++ Protocol/field_type.py
@@ -9,6 +9,6 @@ class Field:
     type = "type"
     body = "body"
     to = "to"
-    id="id"
+    id = "id"
     sender_public_key = "sender_public_key"
     cypher_count = "cypher_count"  # показывает во сколько шифров обернуто body, находящееся на этой же глубине
