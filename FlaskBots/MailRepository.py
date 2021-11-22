999f8e5871bfc6cd89aeeff59e36e84c914eef8e
Сергей Пищулов <57267495+SergeyPishchulov@users.noreply.github.com>
2021-11-22 12:36:40 +0500

    sqlite db using by all servers

--- FlaskBots/MailRepository.py
+++ FlaskBots/MailRepository.py
@@ -1,4 +1,25 @@
 from collections import defaultdict
+from peewee import *
+
+conn = SqliteDatabase('mails.sqlite')
+conn.connect()
+
+
+class BaseModel(Model):
+    class Meta:
+        database = conn
+
+
+class Message(BaseModel):
+    message_id = AutoField(column_name='MessageId')
+    pub_k = TextField(column_name='Pub_K', null=False)
+    text = TextField(column_name='Message_text', null=False)
+
+    class Meta:
+        table_name = 'Message'
+
+
+conn.create_tables([Message])
 
 
 class MailRepository:
@@ -6,7 +27,10 @@ class MailRepository:
         self.storage = defaultdict(set)
 
     def add_message(self, recv_pub_k, message):
-        self.storage[recv_pub_k].add(message)
+        created = Message(pub_k=recv_pub_k, text=message)
+        created.save()
 
     def get_messages_by_recv_pub_k(self, recv_pub_k):
-        return list(self.storage[recv_pub_k])
+        query = Message.select().where(Message.pub_k == recv_pub_k)
+        messages_selected = query.namedtuples().execute()
+        return [record.text for record in messages_selected]
