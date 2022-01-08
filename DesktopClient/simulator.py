bc4abfc58da25c8c2b3310bd1c2350fd275e3684
Сергей Пищулов <57267495+SergeyPishchulov@users.noreply.github.com>
2022-01-08 16:07:17 +0500

    showing messages in desktop. Bug - show all messages multiple times

--- DesktopClient/simulator.py
+++ DesktopClient/simulator.py
@@ -3,6 +3,7 @@ import json
 from nacl.public import PrivateKey
 import time
 
+from DesktopClient.Keys import get_keys_f
 from DesktopClient.multiple_encryption import get_pub_keys
 from Domain import send
 from FlaskBots.MixServer import get_json_dict
@@ -10,10 +11,20 @@ from FlaskBots.Network import get_all_servers
 from Protocol.FieldType import Field
 from utils.coding import pack_k, pack_obj, unpack_obj
 
-private_key = PrivateKey.generate()
-public_key = private_key.public_key
+# # private_key = PrivateKey.generate()
+# # public_key = private_key.public_key
+#
+# private_key = PrivateKey(
+#     b'L\xcf\\\xb5\xb9RSly\x81\xae\xf7\x9b\xdc\xcao\xca\xb1],\xd0$\x03\xbd\xfc\xe6\xd7z\xa4\xdd\xcc\x85')
+# public_key = private_key.public_key
+# print(private_key)
+# print("-------------")
+# print(public_key)
 
-send(public_key, "Oh, hi Mark")
+recv_keys = get_keys_f()
+public_key = recv_keys.public_key
+private_key = recv_keys.private_key
+send(public_key, "Update substitute4")
 
 time.sleep(3)
 
@@ -21,10 +32,10 @@ server = get_all_servers()[0]
 server_pub_k = get_pub_keys()[server]
 message = {"sender_public_key": pack_k(public_key)}
 response = requests.get(url=f"{server}/messages", data=pack_obj(message, server_pub_k))
-print(response)
 d = unpack_obj(data=response.text, sk=private_key)
-print(d)
 for m in d["messages"]:
     encrypted = json.loads(m)
     unp = unpack_obj(encrypted[Field.body], private_key)
+    sender = unp[Field.sender_pub_k]
+    print(sender)
     print(unp)
